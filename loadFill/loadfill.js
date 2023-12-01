d5724f2583485acafc142a06012a0d818c5e475c
Matheus Estevam <100820534+AshPolluXc6@users.noreply.github.com>
2023-12-01 19:33:50 -0300

    Add files via upload

---
+++ loadFill/loadfill.js
@@ -0,0 +1,19 @@
+const count = document.querySelector('.load-count');
+
+// for (let i = 0; i <= 100; i++) {
+//     count.innerHTML += i
+// };
+
+setTimeout (count, 4000)
+
+for(let i = 0; i <= count; i++) {
+    return count + i;
+}
+
+// function() {
+//     var contador =0;
+//     while(contador <= 100) {
+         
+//         contador +=1
+//     }
+// }
