190f9b2f3b54e6b99a2f81c80b4143f3159bd4c2
Matheus Estevam <matheus.oliver.estevam@gmail.com>
2023-12-04 01:12:10 -0300

    Updated all loadFill project

--- loadFill/loadfill.js
+++ loadFill/loadfill.js
@@ -1,19 +1,58 @@
 const count = document.querySelector('.load-count');
+const wavetide = document.querySelectorAll('.waves');
 
-// for (let i = 0; i <= 100; i++) {
-//     count.innerHTML += i
-// };
+const numeros = [
+    0,1,2,3,4,5,6,7,8,9,10,
+    11,12,13,14,15,16,17,18,19,20,
+    21,22,23,24,25,26,27,28,29,30,
+    31,32,33,34,35,36,37,38,39,40,
+    41,42,43,44,45,46,47,48,49,50,
+    51,52,53,54,55,56,57,58,59,60,
+    61,62,63,64,65,66,67,68,69,70,
+    71,72,73,74,75,76,77,78,79,80,
+    81,82,83,84,85,86,87,88,89,90,
+    91,92,93,94,95,96,97,98,99,100
+]
 
-setTimeout (count, 4000)
+let i = -1;
+ 
 
-for(let i = 0; i <= count; i++) {
-    return count + i;
+const intervalId = setInterval(() => {
+    if (i < numeros.length) {
+        count.textContent = numeros[i];
+        i++;
+    } else {
+        clearInterval(intervalId);
+    }
+}, 16);
+
+setTimeout(intervalId, 5000)
+
+// function movetide() {
+//     wavetide.forEach(element => {
+//         element.style.top = '-100px';
+//     });
+// }
+
+// setTimeout(movetide, 2000);
+
+function movetide() {
+    let currentPosition = 340;
+    const targetPosition = -100;
+    const increment = -1; // pode ajustar conforme necessário
+
+    function moveStep() {
+        currentPosition += increment;
+        wavetide.forEach(element => {
+            element.style.top = `${currentPosition}px`;
+        });
+
+        if (currentPosition > targetPosition) {
+            setTimeout(moveStep, 1);
+        }
+    }
+
+    moveStep();
 }
 
-// function() {
-//     var contador =0;
-//     while(contador <= 100) {
-         
-//         contador +=1
-//     }
-// }
+setTimeout(movetide, 0);
